71363668456656ec93f1d6bcb2cd008358d51e32
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:46:29 -0300

    Atualizar o

---
+++ questao35.py
@@ -0,0 +1,13 @@
+print('Soma de elementos de um número inteiro de 4 dígitos')
+print('------------')
+
+numero = int(input(' Digite o número: '))
+
+alg_1 = numero // 1000
+alg_2 = numero % 1000 // 100
+alg_3 = numero % 100 // 10
+alg_4 = numero % 10
+
+print('-------------')
+resultado = alg_1 + alg_2 + alg_3 + alg_4 
+print(f' Resultado = {resultado}')
