fbfa165e5a4e099b25749681603fc2e48ab4473e
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 15:54:41 -0300

    Atualizar o

---
+++ questao32.py
@@ -0,0 +1,15 @@
+print('Soma de um inteiro pelo seu inverso!')
+print('------')
+
+num = int(input('Qual o número? : '))
+
+alg1 = num // 100
+alg2 = num % 100 // 10 
+alg3 = num % 10
+
+resultado1 = (alg1 * 100) + (alg2 * 10) + (alg3)
+resultado2 = (alg3 * 100) + (alg2 * 10) + (alg1)
+resultado_final = resultado1 - resultado2
+
+print('-------')
+print(f'A soma do número {num} com seu inverso é {resultado_final}!')
