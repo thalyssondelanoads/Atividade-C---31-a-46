021e62424b68e3d66fb2dfb4609ae7f36e5536bd
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:43:00 -0300

    Atualizar o

---
+++ questao34.py
@@ -0,0 +1,11 @@
+print('Calcular média de 3 números')
+print('--------')
+
+num1 = float(input('Digite o 1° Numero : '))
+num2 = float(input('Digite o 2° Numero : '))
+num3 = float(input('Digite o 3° Numero : '))
+
+media = round((num1 + num2 + num3)/3,2)
+
+print('---------')
+print(f' A média dos números é {media}! ')
