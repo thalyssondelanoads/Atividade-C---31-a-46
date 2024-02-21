9dc60923b8afdcef121032c086545705c4c9f745
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-20 21:53:01 -0300

    Update questao31.py

--- questao31.py
+++ questao31.py
@@ -1,1 +1,13 @@
-.
+print('Converter um número de 4 dígitos binários para a base decimal')
+print('-----------')
+
+num_bin = int(input('Digite o número binário: '))
+
+num_1 = num_bin // 1000
+num_2 = num_bin % 1000 // 100
+num_3 = num_bin % 100 // 10
+num_4 = num_bin % 10
+resultado = (num_1 * 2**3) + (num_2 * 2**2) + (num_3 * 2**1) + (num_4 * 2**0)
+
+print('------------')
+print(f'O número na base decimal é {resultado}')
