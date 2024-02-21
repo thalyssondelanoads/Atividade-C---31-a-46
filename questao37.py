252c19e25cb37aa86906c7119adec8a858cd0b10
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-20 22:00:43 -0300

    Update questao37.py

--- questao37.py
+++ questao37.py
@@ -1,1 +1,11 @@
-.
+print('Transformar Dias em Anos, Meses e Dias')
+print('-----------')
+
+idade_dias = int(input('Digite a idade em dias: '))
+
+ano = idade_dias // 365
+mes = (idade_dias % 365) // 30
+dia = (idade_dias % 365) % 30
+
+print('-----------')
+print(f'A idade é {ano} anos, {mes} meses e {dia} dias')
