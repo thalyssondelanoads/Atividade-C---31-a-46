57670fbb2038ab962102d3837db2ad6faa1b177b
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-21 22:49:50 -0300

    Update questao38.py

--- questao38.py
+++ questao38.py
@@ -1,1 +1,15 @@
-.
+print('Soma de Frações')
+print('----------')
+
+numerador1 = int(input('Digite o numerador da primeira fração : '))
+denominador1 = int(input('Digite o denominador da primeira fração : '))
+print('----------')
+
+numerador2 = int(input('Digite o numerador da segunda fração : '))
+denominador2 = int(input('Digite o denominador da segunda fração : '))
+
+numerador_soma = numerador1 * denominador2 + numerador2 * denominador1
+denominador_soma = denominador1 * denominador2
+
+print('----------')
+print(f'A soma das frações é : {numerador_soma}/{denominador_soma}')
