ae78fa73175299534d0c1c1a9d359403baae5596
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-21 23:30:54 -0300

    Update questao46.py

--- questao46.py
+++ questao46.py
@@ -1,1 +1,12 @@
-;
+print('Simular Entrada e Parcelas')
+print('----------')
+
+valor_mercadoria = float(input('Qual o valor da mercadoria? '))
+
+entrada = valor_mercadoria // 3
+prestacao = (valor_mercadoria - entrada) // 2
+prestacao += valor_mercadoria - (entrada + 2 * prestacao)
+entrada += prestacao % 2
+
+print('----------')
+print(f'Entrada: R${entrada:.2f}, Prestações (2x): R${prestacao:.2f}')
